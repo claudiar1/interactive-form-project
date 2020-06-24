604a48c6cab406946d0b3ce7d454229ede103595
Claudia Robinson <claudiamegr@gmail.com>
2020-06-24 15:18:09 -0400

    Added validation to name, email, and activities fields

--- js/script.js
+++ js/script.js
@@ -12,6 +12,10 @@ let selectPaymentMethod = document.getElementById("payment");
 let creditCard = document.getElementById("credit-card");
 let paypal = document.getElementById("paypal");
 let bitcoin = document.getElementById("bitcoin");
+let checkboxList = document.querySelectorAll("[type=checkbox");
+let sections = document.getElementsByTagName("fieldset");
+let buttons = document.getElementsByTagName("button");
+let submitButton = buttons[0];
 
 window.onload = function loadFocus() {
   document.getElementById("name").focus();
@@ -80,7 +84,6 @@ activitiesSection[0].addEventListener("change", e => {
 
   totalCostMessage.textContent = `Total Cost: $${totalActivityCost}`;
 
-  let checkboxList = document.querySelectorAll("[type=checkbox");
   let checkedActivityDayAndTime = checkbox.dataset.dayAndTime;
   let checkedActivityName = checkbox.name;
 
@@ -103,7 +106,6 @@ activitiesSection[0].addEventListener("change", e => {
 });
 
 function removePaymentSections(sectionOne, sectionTwo) {
-  let sections = document.getElementsByTagName("fieldset");
   let paymentFieldset = sections[3];
   paymentFieldset.appendChild(creditCard);
   paymentFieldset.appendChild(paypal);
@@ -123,3 +125,96 @@ function paymentSection() {
 }
 
 selectPaymentMethod.addEventListener("change", paymentSection);
+
+function nameValidation(name) {
+  return /^[A-Za-z]+ [A-Za-z]+$/.test(name);
+}
+
+function emailValidation(email) {
+  return /^\w+@\w+\.\w+/i.test(email);
+}
+
+function activitiesValidation() {
+  let checkedOffBoxes = [];
+  for (let i = 0; i < checkboxList.length; i += 1) {
+    if (checkboxList[i].checked) {
+      checkedOffBoxes.push(checkboxList[i]);
+    }
+  }
+  if (checkedOffBoxes.length === 0) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+function cardNumberValidation(cardNumber) {
+  return /^\d{13,16}$/.test(cardNumber);
+}
+
+function zipValidation(zip) {
+  return /^\d{5}$/.test(zip);
+}
+
+function cvvValidation(cvv) {
+  return /^\d{3}$/.test(cvv);
+}
+
+function formValidation(e) {
+  e.preventDefault();
+  let nameField = document.getElementById("name");
+  let emailField = document.getElementById("mail");
+  let bioInfo = sections[0];
+  let emailErrorPresent = document.getElementById("emailError");
+  let nameErrorPresent = document.getElementById("nameError");
+  let activitiesErrorPresent = document.getElementById("activitiesError");
+
+  if (nameValidation(nameField.value) === false && nameErrorPresent === null) {
+    let nameMessage = document.createElement("p");
+    nameMessage.className = "error-message";
+    nameMessage.textContent = "Please enter your first and last name.";
+    nameMessage.id = "nameError";
+    nameField.className = "error";
+    bioInfo.insertBefore(nameMessage, nameField.nextElementSibling);
+  } else {
+    nameField.className = "";
+    if (nameErrorPresent !== null) {
+      bioInfo.removeChild(nameErrorPresent);
+    }
+  }
+
+  if (
+    emailValidation(emailField.value) === false &&
+    emailErrorPresent === null
+  ) {
+    let emailMessage = document.createElement("p");
+    emailMessage.className = "error-message";
+    emailMessage.textContent =
+      "Please enter a correctly formatted email address. Ex: name@company.com";
+    emailMessage.id = "emailError";
+    emailField.className = "error";
+    bioInfo.insertBefore(emailMessage, emailField.nextElementSibling);
+  } else {
+    emailField.className = "";
+    if (emailErrorPresent !== null) {
+      bioInfo.removeChild(emailErrorPresent);
+    }
+  }
+
+  if (activitiesValidation() === false && activitiesErrorPresent === null) {
+    let activitiesMessage = document.createElement("p");
+    activitiesMessage.className = "error-message";
+    activitiesMessage.id = "activitiesError";
+    activitiesMessage.textContent = "Please select at least one activity.";
+    let activitiesTitle = document.getElementById("activities-title");
+    activitiesSection[0].insertBefore(
+      activitiesMessage,
+      activitiesTitle.nextElementSibling
+    );
+  } else {
+    if (activitiesErrorPresent !== null) {
+      activitiesSection[0].removeChild(activitiesErrorPresent);
+    }
+  }
+}
+submitButton.addEventListener("click", formValidation);
